30f25bceddb36a2a9efe5bca2c28b8e4b6b31f2d
Dmitrii Boykov <boikovdmitriy@mail.ru>
2021-08-26 21:26:47 +0400

    [ADD] added base project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+import {Home} from "./pages/home";
+import {WeatherPage} from "./pages/weatherPage";
+
+function App() {
+  return (
+        <Switch>
+          <Route path="/city/:city" >
+              <WeatherPage />
+          </Route>
+          <Route path="/" >
+              <Home />
+          </Route>
+        </Switch>
+  );
+}
+
+export default App;
