e5fdc2c17443985f8b825c8fb54ef36ad8c6599d
Dmitrii Boykov <boikovdmitriy@mail.ru>
2021-09-02 18:10:51 +0400

    [ADD] added linter

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,58 +1,45 @@
-import React, {useCallback, useState} from 'react';
-import {Box, Button, createStyles, makeStyles, TextField} from "@material-ui/core";
-import {useHistory} from "react-router-dom";
+import React, { useCallback, useState } from 'react';
+import { Box, Button, createStyles, makeStyles, TextField } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-const useStyles = makeStyles(() => createStyles({
+const useStyles = makeStyles(() =>
+  createStyles({
     form: {
-        background: `#e4e4e4c7`,
-        padding: 30,
-        borderRadius: 15,
-        minWidth: '60%',
-        maxWidth: '600px'
-    }
-}))
+      background: `#e4e4e4c7`,
+      padding: 30,
+      borderRadius: 15,
+      minWidth: '60%',
+      maxWidth: '600px',
+    },
+  }),
+);
 
 export const Home = () => {
-    const styles = useStyles();
-    const history = useHistory();
-    const [value, setValue] = useState('');
+  const styles = useStyles();
+  const history = useHistory();
+  const [value, setValue] = useState('');
 
-    const handleOnChange = useCallback(({target: {value}}: any) => {
-        setValue(value);
-    }, [setValue])
+  const handleOnChange = useCallback(
+    ({ target: { value: newValue } }: any) => {
+      setValue(newValue);
+    },
+    [setValue],
+  );
 
-    const handleSelect = () => {
-        history.push(`/city/${value}`)
-    };
+  const handleSelect = () => {
+    if (value !== '') history.push(`/city/${value}`);
+  };
 
-    return (
-        <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            width="100%"
-            height="100%"
-        >
-            <Box
-                display="flex"
-                flexDirection="column"
-                className={styles.form}
-            >
-                <TextField
-                    value={value}
-                    onChange={handleOnChange}
-                    label="Enter a city"
-                    fullWidth
-                />
-                <Box marginTop={2}>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        onClick={handleSelect}
-                    >Show weather</Button>
-                </Box>
-            </Box>
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center" width="100%" height="100%">
+      <Box display="flex" flexDirection="column" className={styles.form}>
+        <TextField value={value} onChange={handleOnChange} label="Enter a city" fullWidth />
+        <Box marginTop={2}>
+          <Button variant="contained" color="primary" fullWidth onClick={handleSelect}>
+            Show weather
+          </Button>
         </Box>
-    )
-}
+      </Box>
+    </Box>
+  );
+};
--- src/pages/weatherPage/index.tsx
+++ src/pages/weatherPage/index.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
-import {Box, createStyles, makeStyles} from "@material-ui/core";
-import {useLocation} from "react-router-dom";
+import { Box, Button, createStyles, makeStyles } from '@material-ui/core';
+import { useHistory, useLocation } from 'react-router-dom';
 
-const useStyles = makeStyles(() => createStyles({
+const useStyles = makeStyles(() =>
+  createStyles({
     form: {
-        background: `#e4e4e4c7`,
-        padding: 30,
-        borderRadius: 15,
-        minWidth: '60%',
-        maxWidth: '600px'
-    }
-}))
+      background: `#e4e4e4c7`,
+      padding: 30,
+      borderRadius: 15,
+      minWidth: '60%',
+      maxWidth: '600px',
+    },
+  }),
+);
 
 export const WeatherPage = () => {
-    const styles = useStyles();
-    const location = useLocation();
+  const styles = useStyles();
+  const location = useLocation();
+  const history = useHistory();
 
-    return (
-        <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            width="100%"
-            height="100%"
-        >
-            <Box
-                display="flex"
-                flexDirection="column"
-                className={styles.form}
-            >
-                {JSON.stringify(location)}
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center" width="100%" height="100%">
+      <Button onClick={() => history.push('/')}>Back</Button>
+      <Box display="flex" flexDirection="column" className={styles.form}>
+        {JSON.stringify(location)}
+      </Box>
+    </Box>
+  );
+};
